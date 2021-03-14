d22ffc2dff982d0553a341eae4cc4acc2c44c505
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2021-03-14 19:49:37 +0200

    Завдання 4, завдання 5 Варіант 8

---
+++ Laboratorna1Variant 8/Laboratorna1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorna1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+        
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+        }
+       
+    private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+           
+            this.Size = new Size(1000,500);
+        }
+    }
+}
---
+++ zavdana4/zavdana4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zavdana4
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Text = "Label" + count.ToString();
+            label.Font = new Font("Arial", 10, FontStyle.Bold);
+            label.Location = new Point(50 * count, 50 * count);
+            this.Controls.Add(label);
+            count++;
+        }
+    }
+}
